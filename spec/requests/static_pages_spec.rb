7566c4cfb8f3f2f53f9104e72f491b1d8039164e
Ozcan Tercan <otercan88@gmail.com>
2014-02-26 17:31:43 +0000

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,20 +2,26 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
-
-      it "should have the content 'Sample App'" do
-            visit '/static_pages/home'
-                  expect(page).to have_content('Sample App')
-                      end
-
-                          it "should have the title 'Home'" do
-                                visit '/static_pages/home'
-                                      expect(page).to have_title("Sample App | Home")
-                                          end
-                                            end
-
-                                              describe "Help page" do
+                                              describe "Home page" do
+                                              
+                                                  it "should have the content 'Sample App'" do
+                                                        visit '/static_pages/home'
+                                                              expect(page).to have_content('Sample App')
+                                                                  end
+                                                                  
+                                                                      it "should have the base title" do
+                                                                            visit '/static_pages/home'
+                                                                                  expect(page).to have_title("Sample App")
+                                                                                      end
+                                                                                      
+                                                                                          it "should not have a custom page title" do
+                                                                                                visit '/static_pages/home'
+                                                                                                      expect(page).not_to have_title('| Home')
+                                                                                                          end
+                                                                                                            end
+                                                                                                            
+                                                                                                            
+ describe  "Help page" do
 
                                                   it "should have the content 'Help'" do
                                                         visit '/static_pages/help'
